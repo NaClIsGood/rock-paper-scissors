1123113cfb5b085d9eb3c00c0eb2cbd88390e6e2
John Tran <johntr2002@gmail.com>
2022-10-04 16:48:51 -0400

    Added player and computer variables

--- game.js
+++ game.js
@@ -2,10 +2,12 @@
 // 1 = paper //
 // 2 = scissors //
 
-
+let playChoice;
+let compChoice;
 
 function getComputerChoice(){
     return Math.floor(Math.random()*3)
 }
 
 console.log(getComputerChoice);
+
